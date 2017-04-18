e4e9361a5e323c227a312d9c32521f2bd26e65c8
RebeccaK375 <krebecca@mit.edu>
2017-04-18 09:48:54 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ProductPage.css

--- src/App.js
+++ src/App.js
@@ -89,4 +89,7 @@ class App extends Component {
   }
 }
 
+
+
+
 export default App;
--- src/StorePage.js
+++ src/StorePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './StorePage.css';
 
+
 class Product extends Component {
   render() {
     return (
