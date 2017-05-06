199997d3ee776d04c9e9f92110672c45513d75c0
Tomas Gudmundsson <lavenderviking@Tomass-MacBook-Pro.local>
2017-05-06 03:38:35 -0400

    Continent sales distribution finished

--- src/App.js
+++ src/App.js
@@ -48,49 +48,66 @@ class App extends Component {
           media: [image1],
           price: '$98.00',
           uid: 'bag1',
-          description: "Fancy shmancy straw for divas! Available only in beige and black, but fabulous with any outfits."
-
+          description: "Fancy shmancy straw for divas! Available only in beige and black, but fabulous with any outfits.",
+          continentSales: ['1500', '50000', '3000', '0', '0', '200'],
+          continentSalesTxt: ['1.500', '50.000', '3.000', '0', '0', '200']
         }, {
           name: 'Handwoven Picnic Bag',
           media: [image2],
           price: '$87.00',
           uid: 'bag2',
-          description: "Fancy shmancy straw for divas! Available only in beige and black, but fabulous with any outfits."
+          description: "Fancy shmancy straw for divas! Available only in beige and black, but fabulous with any outfits.",
+          continentSales: ['20500', '5000', '1000', '500', '0', '20000'],
+          continentSalesTxt: ['25.000', '5.000', '1.000', '500', '0', '20.000']
         }, {
           name: 'Red Glamour Bag',
           media: [image3],
           price: '$76.00',
-          uid: 'bag3'
+          uid: 'bag3',
+          continentSales: ['65', '1000', '33000', '60000', '6000', '2500'],
+          continentSalesTxt: ['65', '1.000', '33.000', '60.000', '6.000', '2.500']
         }, {
           name: 'Brown Pull-Over Tote',
           media: [image4],
           price: '$55.00',
-          uid: 'bag4'
+          uid: 'bag4',
+          continentSales: ['15000', '500', '21000', '870', '0', '2000'],
+          continentSalesTxt: ['15.000', '500', '21.000', '870', '0', '2.000']
         }, {
           name: 'Blue Chic Handbag',
           media: [image5],
           price: '$76.00',
-          uid: 'bag5'
+          uid: 'bag5',
+          continentSales: ['1500', '50000', '3000', '0', '0', '200'],
+          continentSalesTxt: ['1.500', '50.000', '3.000', '0', '0', '200']
         }, {
           name: 'Summer Orange Handbag',
           media: [image6],
           price: '$88.00',
-          uid: 'bag6'
+          uid: 'bag6',
+          continentSales: ['80000', '900', '39000', '15500', '7700', '0'],
+          continentSalesTxt: ['80.000', '900', '39.000', '15.500', '7.700', '0']
         }, {
           name: 'Banana Picnic Bag',
           media: [image7],
           price: '$109.00',
-          uid: 'bag7'
+          uid: 'bag7',
+          continentSales: ['190', '50000', '30000', '5000', '150000', '2000'],
+          continentSalesTxt: ['190', '50.000', '30.000', '5.000', '150.000', '2.000']
         }, {
           name: 'Pretty and Purple Handbag',
           media: [image8],
           price: '$76.00',
-          uid: 'bag8'
+          uid: 'bag8',
+          continentSales: ['47000', '8000', '3500', '0', '2570', '53000'],
+          continentSalesTxt: ['47.000', '8.000', '3.500', '0', '2.570', '53.000']
         }, {
           name: 'Classic Picnic Bag',
           media: [image9],
           price: '$56.00',
-          uid: 'bag9'
+          uid: 'bag9',
+          continentSales: ['11500', '50', '300', '8000', '0', '2500'],
+          continentSalesTxt: ['11.500', '50', '3000', '8.000', '0', '2.500']
         }
       ],
     }
@@ -104,6 +121,9 @@ class App extends Component {
         media: detail.media,
         price: detail.price,
         uid: detail.uid,
+        filter: detail.filter,
+        continentSales: detail.continentSales,
+        continentSalesTxt: detail.continentSalesTxt
       }]),
     });
   }
